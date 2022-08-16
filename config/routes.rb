d390a71cb00b227f45a444052540e7fc13c81351
Juliana Bonardi <julicbonardi@hotmail.com>
2022-08-16 15:35:12 -0300

    Functional, however is missing front-end

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,24 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # root 'tasks#index'
+
+  # # Read all
+  # get 'tasks', to: 'tasks#index'
+
+  # # Create new task
+  # get 'tasks/new', to: 'tasks#new'
+  # post 'tasks', to: 'tasks#create'
+
+  # # Read for id
+  # get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  # # Update
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # # Delete
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
+  resources :tasks
 end
